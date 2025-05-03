263cd2105ea3b6da2dfa2fa03c468403e40c9875
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2025-05-04 06:44:07 +0800

    fix(app): apply padding to main content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,27 +9,28 @@
   const entries = liveQuery(async () => await db.entry.toArray());
 </script>
 
-<CreateEntryForm />
-
-<div class="overflow-x-auto">
-  <Table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Amount</th>
-        <th>Creation Time</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each entries.current as { id, name, amount, createdAt } (id)}
+<main class="space-y-4 p-4">
+  <CreateEntryForm />
+  <div class="overflow-x-auto">
+    <Table>
+      <thead>
         <tr>
-          <td>{id}</td>
-          <td>{name}</td>
-          <td>{amount}</td>
-          <td>{createdAt.toLocaleString()}</td>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Amount</th>
+          <th>Creation Time</th>
         </tr>
-      {/each}
-    </tbody>
-  </Table>
-</div>
+      </thead>
+      <tbody>
+        {#each entries.current as { id, name, amount, createdAt } (id)}
+          <tr>
+            <td>{id}</td>
+            <td>{name}</td>
+            <td>{amount}</td>
+            <td>{createdAt.toLocaleString()}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </Table>
+  </div>
+</main>
