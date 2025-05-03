dcd7eba08c61535e0b65aafc30208d4690e82ec3
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2025-05-04 04:01:58 +0800

    feat(app): add basic button and input wrapper components

---
+++ src/lib/ui/button.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import { type VariantProps, cva } from 'class-variance-authority';
+  import { twMerge } from 'tailwind-merge';
+
+  import type { HTMLButtonAttributes } from 'svelte/elements';
+
+  const button = cva(['btn'], {
+    variants: {
+      color: {
+        neutral: 'btn-neutral',
+        primary: 'btn-primary',
+        secondary: 'btn-secondary',
+        accent: 'btn-accent',
+        info: 'btn-info',
+        success: 'btn-success',
+        warning: 'btn-warning',
+        error: 'btn-error',
+      },
+      variant: {
+        outline: 'btn-outline',
+        dash: 'btn-dash',
+        soft: 'btn-soft',
+        ghost: 'btn-ghost',
+        link: 'btn-link',
+      },
+      behavior: {
+        active: 'btn-active',
+        disabled: 'btn-disabled',
+      },
+      size: {
+        xs: 'btn-xs',
+        sm: 'btn-sm',
+        md: 'btn-md',
+        lg: 'btn-lg',
+        xl: 'btn-xl',
+      },
+      modifier: {
+        wide: 'btn-wide',
+        block: 'btn-block',
+        square: 'btn-square',
+        circle: 'btn-circle',
+      },
+    },
+  });
+
+  type Props = VariantProps<typeof button> & HTMLButtonAttributes;
+  const {
+    color,
+    variant,
+    behavior,
+    size,
+    modifier,
+    class: className,
+    children,
+    ...props
+  }: Props = $props();
+  const classes = $derived(
+    twMerge(button({ color, variant, behavior, size, modifier, className })),
+  );
+</script>
+
+<button class={classes} {...props}>{@render children?.()}</button>
---
+++ src/lib/ui/input.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { type VariantProps, cva } from 'class-variance-authority';
+  import { twMerge } from 'tailwind-merge';
+
+  import type { HTMLInputAttributes } from 'svelte/elements';
+
+  const input = cva(['input'], {
+    variants: {
+      variant: {
+        ghost: 'input-ghost',
+      },
+      color: {
+        neutral: 'input-neutral',
+        primary: 'input-primary',
+        secondary: 'input-secondary',
+        accent: 'input-accent',
+        info: 'input-info',
+        success: 'input-success',
+        warning: 'input-warning',
+        error: 'input-error',
+      },
+      size: {
+        xs: 'input-xs',
+        sm: 'input-sm',
+        md: 'input-md',
+        lg: 'input-lg',
+        xl: 'input-xl',
+      },
+    },
+  });
+
+  type Props = VariantProps<typeof input> & HTMLInputAttributes;
+  const { variant, color, size, class: className, ...props }: Props = $props();
+  const classes = $derived(twMerge(input({ color, variant, size, className })));
+</script>
+
+<input class={classes} {...props} />
